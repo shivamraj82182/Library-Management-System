6ff921538b12bd98617af26e236d87c418c8858f
Shivam Rajpoot <shivamraj82182@gmail.com>
2026-09-03 23:37:07 +0530

    Fix Gmail SMTP port

--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -1,25 +1,22 @@
-// export default sendOtp;
 import { createTransport } from "nodemailer";
 
 const sendOtp = async (email, otp) => {
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    console.log("EMAIL_USER:", process.env.EMAIL_USER);
-    console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-
-    const transporter = createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: "Your OTP Code",
-        html: `<h2>Your OTP is ${otp}</h2>`
-    });
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Your OTP Code",
+    html: `<h2>Your OTP is ${otp}</h2>`,
+  });
 };
 
 export default sendOtp;
