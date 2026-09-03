f5792f2ef4c247477dd16b3916214a27101a7622
Shivam Rajpoot <shivamraj82182@gmail.com>
2026-09-03 23:48:55 +0530

    Force IPv4 for Gmail SMTP

--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -1,22 +1,28 @@
 import { createTransport } from "nodemailer";
 
 const sendOtp = async (email, otp) => {
-  const transporter = createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Your OTP Code",
-    html: `<h2>Your OTP is ${otp}</h2>`,
-  });
+    const transporter = createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        family: 4,
+
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    await transporter.sendMail({
+        from: `"Library Management System" <${process.env.EMAIL_USER}>`,
+        to: email,
+        subject: "Your OTP Code",
+        html: `
+            <h2>Your OTP is ${otp}</h2>
+            <p>Your OTP for password reset is <strong>${otp}</strong>.</p>
+        `,
+    });
 };
 
 export default sendOtp;
